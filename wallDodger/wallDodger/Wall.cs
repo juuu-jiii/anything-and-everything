67ba4a260b5a2683403e71b0e4c45431fbb6e564
juuu-jiii <juuu-jiii@users.noreply.github.com>
2020-05-15 02:13:15 +0800

    Added Wall and (incomplete) WallManager classes.

---
+++ wallDodger/wallDodger/Wall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace wallDodger
+{
+	class Wall
+	{
+		// Fields
+		private Texture2D wallAsset;
+		private const int WallWidth = 100;
+		private const int WallHeight = 35;
+		private Vector2 Position { get; set; }
+
+		// Parameterised constructor
+		public Wall(Texture2D wallAsset, int x, int y)
+		{
+			this.wallAsset = wallAsset;
+			Position = new Vector2(x, y);
+		}
+
+		/// <summary>
+		/// Draws Wall objects to the screen.
+		/// </summary>
+		/// <param name="spriteBatch">
+		/// The SpriteBatch object used to draw with.
+		/// </param>
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw
+				(
+				wallAsset,
+				new Rectangle((int)Position.X, (int)Position.Y, WallWidth, WallHeight),
+				Color.White
+				);
+		}
+	}
+}
---
+++ wallDodger/wallDodger/WallManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace wallDodger
+{
+	class WallManager
+	{
+		// Private static field to hold one instance of this class - singleton
+		private static WallManager wallManagerInstance;
+
+		private LinkedList<Wall> rightWalls;
+		private LinkedList<Wall> leftWalls;
+		private Random generator;
+		private Point spawner;
+		private Point despawner;
+		private int ScrollSpeed { get; set; }
+		private int GapSize { get; set; }
+
+		// Singleton pattern: private constructor
+		private WallManager(int windowHeight)
+		{
+			rightWalls = new LinkedList<Wall>();
+			leftWalls = new LinkedList<Wall>();
+			generator = new Random();
+
+			// Spawner and despawner are above and below screen, respectively.
+			spawner = new Point(0, -50);
+			despawner = new Point(0, windowHeight + 50);
+
+			ScrollSpeed = 20;
+			GapSize = 200;
+		}
+
+		// Public static get property that returns an instance of the class, 
+		//		instantiating an object if not already done.
+		public static WallManager WallManagerInstance
+		{
+			get
+			{
+				if (wallManagerInstance == null)
+				{
+					wallManagerInstance = new WallManager(Game1.WindowHeight);
+					return wallManagerInstance;
+				}
+				else
+				{
+					return wallManagerInstance;
+				}
+			}
+		}
+
+		public void SpawnDespawnWallPair()
+	}
+}
